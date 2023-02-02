9b9405c76a2c140fefcdd71f6621e4aedd5bddd2
gathonishazzy <122537404+gathonishazzy@users.noreply.github.com>
2023-02-02 16:51:31 +0300

    Add files via upload

---
+++ divisibility5.c
@@ -0,0 +1,14 @@
+//program to determine whether a number is divisible by 5
+#include<stdio.h>
+int main ()
+{
+  int a;
+  printf("enter the value of a :");
+  scanf("%d",&a);
+  if(a%5==0){
+    printf("a is divisible by 5");}
+  else{
+    printf("a is not divisible by 5");}
+  return 0;
+  }
+  
