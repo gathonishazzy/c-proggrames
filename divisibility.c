cd3dafb383d833bbec37f15f3c356b179edb5f0b
gathonishazzy <122537404+gathonishazzy@users.noreply.github.com>
2023-02-02 15:23:12 +0300

    Add files via upload

---
+++ divisibility.c
@@ -0,0 +1,13 @@
+//determine whether a number is odd or even
+#include<stdio.h>
+int main ()
+{
+  int x;
+  printf("\n enter value of x:");
+  scanf("%d",&x);
+  if (x%2==0){
+    printf("\n x is even");
+  }
+else(printf("x is odd"));
+return 0;
+}
