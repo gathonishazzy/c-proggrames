a91df5ad9d5ab33a59074269044a65847ded6305
gathonishazzy <122537404+gathonishazzy@users.noreply.github.com>
2023-02-23 15:54:11 +0300

    Add files via upload

---
+++ numbers.c
@@ -0,0 +1,14 @@
+//natural numbers
+#include <stdio.h>
+int main() 
+{
+  int n=100;
+  while(n>9){
+    printf("%d ",n);
+    n=n-1;
+    }
+  printf("\nfor loop body finished");
+  return 0;
+  }  
+
+  
