3e8a8ae794fb108b7591be29b09ff3ef60e273e6
gathonishazzy <122537404+gathonishazzy@users.noreply.github.com>
2023-03-01 14:42:42 +0300

    Add files via upload

---
+++ volume.c
@@ -0,0 +1,16 @@
+//function of volume of a cylinder
+#include<stdio.h>
+int volume();
+int main()
+{
+int answer=volume();
+printf("\nThe volume of a cylinder is:%d",answer);
+return 0;
+}
+int volume()
+{
+int pi=3.142,r,h;
+printf("\nEnter the radius and the height:");
+scanf("%d%d",&r,&h);
+return pi*r*r*h;
+}
